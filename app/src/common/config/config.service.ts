a81579c805f661896cd0158a012af10aa34f2856
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-09-10 17:00:56 +0300

    Get config values from environment in config model classes

--- app/src/common/config/config.service.ts
+++ app/src/common/config/config.service.ts
@@ -1,31 +1,19 @@
 import { Injectable, Logger } from "@nestjs/common";
-import { plainToInstance } from "class-transformer";
 import { validateSync } from "class-validator";
 import { InvalidConfigurationException } from "../exceptions/invalid-configuration.exception";
 import { KaikuAppConfiguration } from "./kaiku-app-configuration.model";
 
 @Injectable()
 export class ConfigService {
-  public readonly config: KaikuAppConfiguration;
+  public readonly config = new KaikuAppConfiguration();
   private readonly logger = new Logger(ConfigService.name);
 
   constructor() {
     // Validate in constructor to exit as early as possible upon invalid configuration.
-    const config = this.init();
-    this.validate(config);
-    this.config = config;
+    this.validate(this.config);
   }
 
-  init(): KaikuAppConfiguration {
-    const env: Partial<KaikuAppConfiguration> = {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      nodeEnv: (process.env.NODE_ENV as any) || "development",
-    };
-
-    return plainToInstance(KaikuAppConfiguration, env);
-  }
-
-  validate(config: KaikuAppConfiguration): void {
+  private validate(config: KaikuAppConfiguration): void {
     const errors = validateSync(config);
 
     if (errors.length) {
