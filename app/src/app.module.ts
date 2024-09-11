e68200e4cec9913cfe5d5b1f2f9127cef861e17c
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-09-11 07:27:27 +0300

    Copy over presence module from Hybridilusmu

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -3,10 +3,19 @@ import { BoltModule } from "./bolt/bolt.module";
 import { ConfigModule } from "./common/config/config.module";
 import { DatabaseModule } from "./database/database.module";
 import { OfficeModule } from "./entities/office/office.module";
+import { PresenceModule } from "./entities/presence/presence.module";
 import { UserSettingsModule } from "./entities/user-settings/user-settings.module";
 import { UserModule } from "./entities/user/user.module";
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, BoltModule, OfficeModule, UserSettingsModule, UserModule],
+  imports: [
+    ConfigModule,
+    DatabaseModule,
+    BoltModule,
+    OfficeModule,
+    UserSettingsModule,
+    UserModule,
+    PresenceModule,
+  ],
 })
 export class AppModule {}
