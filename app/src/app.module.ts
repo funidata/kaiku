3ee0b1db0b17142580f084adfeaf69a85b85b61c
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-09-13 18:01:18 +0300

    Copy over GUI module from Hybridilusmu

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -3,9 +3,10 @@ import { BoltModule } from "./bolt/bolt.module";
 import { ConfigModule } from "./common/config/config.module";
 import { DatabaseModule } from "./database/database.module";
 import { EntitiesModule } from "./entities/entities.module";
+import { GuiModule } from "./gui/gui.module";
 import { SyncModule } from "./sync/sync.module";
 
 @Module({
-  imports: [ConfigModule, DatabaseModule, BoltModule, EntitiesModule, SyncModule],
+  imports: [ConfigModule, DatabaseModule, BoltModule, EntitiesModule, SyncModule, GuiModule],
 })
 export class AppModule {}
