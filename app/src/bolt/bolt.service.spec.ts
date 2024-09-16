f0d4864732832d325a4d5ac01c4f47abf479e729
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-09-16 15:14:30 +0300

    Add unit tests for Bolt service

---
+++ app/src/bolt/bolt.service.spec.ts
@@ -0,0 +1,38 @@
+import { Test } from "@nestjs/testing";
+import { App } from "@slack/bolt";
+import { StringIndexed } from "@slack/bolt/dist/types/helpers";
+import { ConfigServiceMock } from "../../test/mocks/config-service.mock";
+import { BoltService } from "./bolt.service";
+
+jest.mock("@slack/bolt");
+
+describe("BoltService", () => {
+  let boltService: BoltService;
+
+  beforeEach(async () => {
+    const app = await Test.createTestingModule({
+      providers: [BoltService, ConfigServiceMock],
+    }).compile();
+
+    boltService = app.get<BoltService>(BoltService);
+  });
+
+  describe("Slack connection", () => {
+    it("Connection is initialized", async () => {
+      await boltService.connect();
+      expect(boltService.getBolt()).not.toBeUndefined();
+      expect(boltService.getBolt().start).toHaveBeenCalledTimes(1);
+    });
+
+    it("Connection is closed cleanly", async () => {
+      await boltService.connect();
+      await boltService.disconnect();
+      expect(boltService["bolt"].stop).toHaveBeenCalledTimes(1);
+    });
+
+    it("Returns Bolt instance", async () => {
+      await boltService.connect();
+      expect(boltService.getBolt()).toBeInstanceOf(App<StringIndexed>);
+    });
+  });
+});
