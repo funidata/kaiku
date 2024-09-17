9fb47fead21a1ee55acccc9984289c1cd3d04cc3
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-09-17 10:36:28 +0300

    Remove current user decorator
    Nest.js param decorators won't work in Bolt handlers as they are bound to context before the param decorator function would have a chance to fire.

--- app/src/bolt/decorators/bolt-user.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { ExecutionContext, createParamDecorator } from "@nestjs/common";
-
-export const BoltUser = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest();
-  console.log(data);
-  console.log(request);
-  return request.user;
-});
