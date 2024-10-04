54a7c133aaaa2bd2aeed5065f674e9eba7f24bed
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-10-04 12:26:39 +0300

    Add common controls to all home tab views

--- app/src/gui/home-tab/home-tab.builder.ts
+++ app/src/gui/home-tab/home-tab.builder.ts
@@ -1,36 +1,21 @@
 import { Injectable } from "@nestjs/common";
-import { Header, HomeTab, ViewBlockBuilder } from "slack-block-builder";
+import { HomeTab, ViewBlockBuilder } from "slack-block-builder";
 import { Appendable } from "slack-block-builder/dist/internal";
 import { AppHomeOpenedArgs } from "../../bolt/types/app-home-opened.type";
 import { BoltActionArgs } from "../../bolt/types/bolt-action-args.type";
 import { ViewsPublishResponse } from "../../bolt/types/views-publish-response.type";
 import { HomeTabControls } from "./home-tab-controls";
-import { DayList } from "./views/registration/day-list.builder";
-import { VisibleOfficeSelect } from "./views/registration/visible-office-select.builder";
 
 @Injectable()
 export class HomeTabBuilder {
-  constructor(
-    private dayListBlocks: DayList,
-    private visibleOfficeSelectBuilder: VisibleOfficeSelect,
-    private homeTabControls: HomeTabControls,
-  ) {}
+  constructor(private homeTabControls: HomeTabControls) {}
 
-  async buildBlocks(userId: string): Promise<Appendable<ViewBlockBuilder>> {
+  async build(content: Appendable<ViewBlockBuilder>) {
     const controls = this.homeTabControls.build();
-    const officeSelect = await this.visibleOfficeSelectBuilder.buildBlocks();
-    const dayList = await this.dayListBlocks.buildBlocks(userId);
 
-    return [...controls, Header({ text: "Ilmoittautuminen" }), ...officeSelect, ...dayList];
-  }
-
-  async build(blocks: Appendable<ViewBlockBuilder>) {
-    return (
-      HomeTab()
-        // TODO: Needs "global" items.
-        .blocks(...blocks)
-        .buildToObject()
-    );
+    return HomeTab()
+      .blocks(...controls, ...content)
+      .buildToObject();
   }
 
   async publish(
