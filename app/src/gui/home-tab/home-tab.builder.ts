4d3436fa2ec1af3037613095eb349a1ca38b276e
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-10-04 09:25:15 +0300

    Move home tab controls to proper service

--- app/src/gui/home-tab/home-tab.builder.ts
+++ app/src/gui/home-tab/home-tab.builder.ts
@@ -1,12 +1,11 @@
 import { Injectable } from "@nestjs/common";
 import { BlockAction } from "@slack/bolt";
-import { Actions, Button, Header, HomeTab, ViewBlockBuilder } from "slack-block-builder";
+import { Header, HomeTab, ViewBlockBuilder } from "slack-block-builder";
 import { Appendable, SlackHomeTabDto } from "slack-block-builder/dist/internal";
-import Action from "../../bolt/enums/action.enum";
 import { AppHomeOpenedArgs } from "../../bolt/types/app-home-opened.type";
 import { ViewsPublishResponse } from "../../bolt/types/views-publish-response.type";
 import { WebClient } from "../../bolt/types/web-client.type";
-import { DevUiBuilder } from "../dev/dev-ui.builder";
+import { HomeTabControls } from "./home-tab-controls";
 import { DayList } from "./views/day-list.builder";
 import { VisibleOfficeSelect } from "./views/visible-office-select.builder";
 
@@ -14,25 +13,16 @@ import { VisibleOfficeSelect } from "./views/visible-office-select.builder";
 export class HomeTabBuilder {
   constructor(
     private dayListBlocks: DayList,
-    private devToolsBuilder: DevUiBuilder,
     private visibleOfficeSelectBuilder: VisibleOfficeSelect,
+    private homeTabControls: HomeTabControls,
   ) {}
 
   async buildBlocks(userId: string): Promise<Appendable<ViewBlockBuilder>> {
-    const devTools = this.devToolsBuilder.buildBlocks();
+    const controls = this.homeTabControls.build();
     const officeSelect = await this.visibleOfficeSelectBuilder.buildBlocks();
     const dayList = await this.dayListBlocks.buildBlocks(userId);
-    return [
-      ...devTools,
-      Actions().elements([
-        Button({ text: "Ilmoittautuminen" }),
-        Button({ text: "Läsnäolijat", actionId: Action.OPEN_PRESENCE_VIEW }),
-        Button({ text: "Asetukset" }),
-      ]),
-      Header({ text: "Ilmoittautuminen" }),
-      ...officeSelect,
-      ...dayList,
-    ];
+
+    return [...controls, Header({ text: "Ilmoittautuminen" }), ...officeSelect, ...dayList];
   }
 
   // TODO: Remove
