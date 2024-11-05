d301107c8a120d03a0174ccc956f904d58d0c90c
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-11-05 13:34:48 +0200

    Custom rich text builder

--- app/src/gui/home-tab/views/presence/presence-list.ts
+++ app/src/gui/home-tab/views/presence/presence-list.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { Header } from "slack-block-builder";
 import { Appendable, ViewBlockBuilder } from "slack-block-builder/dist/internal";
 import { Presence } from "../../../../entities/presence/presence.model";
+import { RichText } from "../../../block-builders/rich-text.builder";
 
 type PresenceSearchResult = {
   date: Date;
@@ -13,12 +14,7 @@ export class PresenceList {
   build(results: PresenceSearchResult[]): Appendable<ViewBlockBuilder> {
     return results.flatMap((result) => [
       Header({ text: "date" }),
-      {
-        build: () => ({
-          type: "rich_text",
-          elements: [this.listFromEntries(result.entries)],
-        }),
-      } as any,
+      RichText().elements(this.listFromEntries(result.entries)),
     ]);
   }
 
