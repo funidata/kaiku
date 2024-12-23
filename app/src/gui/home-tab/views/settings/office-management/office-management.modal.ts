7495f2bcfb63a462295481c538540e0612619123
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-12-23 10:58:31 +0200

    List of offices

--- app/src/gui/home-tab/views/settings/office-management/office-management.modal.ts
+++ app/src/gui/home-tab/views/settings/office-management/office-management.modal.ts
@@ -1,12 +1,31 @@
 import { Injectable } from "@nestjs/common";
-import { Header, Modal } from "slack-block-builder";
+import { Actions, Button, Modal, Section } from "slack-block-builder";
 import { SlackModalDto } from "slack-block-builder/dist/internal";
+import { Office } from "../../../../../entities/office/office.model";
+import { OfficeService } from "../../../../../entities/office/office.service";
 
 @Injectable()
 export class OfficeManagementModal {
+  constructor(private officeService: OfficeService) {}
+
   async build(): Promise<Readonly<SlackModalDto>> {
+    const offices = await this.officeService.findAll();
+
     return Modal({ title: "Toimistojen hallinta" })
-      .blocks([Header({ text: "asd" })])
+      .blocks([
+        ...this.buildOfficeList(offices),
+        Actions().elements(Button({ text: "Lisää uusi toimisto" })),
+      ])
       .buildToObject();
+  }
+
+  private buildOfficeList(offices: Office[]) {
+    if (offices.length === 0) {
+      return [Section({ text: "Yhtään toimistoa ei ole lisätty Kaikuun." })];
+    }
+
+    return offices.map((office) =>
+      Section({ text: office.name }).accessory(Button({ text: "Muokkaa" })),
+    );
   }
 }
--- app/src/gui/home-tab/views/settings/office-management/office-management.module.ts
+++ app/src/gui/home-tab/views/settings/office-management/office-management.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { OfficeModule } from "../../../../../entities/office/office.module";
 import { OfficeManagementController } from "./office-management.controller";
 import { OfficeManagementModal } from "./office-management.modal";
 
 @Module({
+  imports: [OfficeModule],
   providers: [OfficeManagementModal],
   controllers: [OfficeManagementController],
 })
