7ce91dd638d4677e4249110f438c813865fcf934
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-12-31 10:55:38 +0200

    Add some comments

--- app/src/database/migrations/1735633836694-replace-presence-type-with-remote-column.ts
+++ app/src/database/migrations/1735633836694-replace-presence-type-with-remote-column.ts
@@ -2,6 +2,11 @@ import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 
 export class ReplacePresenceTypeWithRemoteColumn1735633836694 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    /*
+     * N.B.: Values are not migrated from `type` to `remote` because Kaiku is
+     * not running anywhere yet and the default value of `remote`, while
+     * sometimes incorrect, will not break anything.
+     */
     const table = await queryRunner.getTable("presence");
 
     await queryRunner.dropColumn(table, "type");
