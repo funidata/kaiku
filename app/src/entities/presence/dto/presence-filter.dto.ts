c7664c60b1d40e87995921c67806dfe0350d9b6a
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-12-31 10:41:54 +0200

    Remove presence type enumeration

--- app/src/entities/presence/dto/presence-filter.dto.ts
+++ app/src/entities/presence/dto/presence-filter.dto.ts
@@ -1,10 +1,8 @@
-import { PresenceType } from "../presence.model";
-
 export type PresenceFilter = {
   date?: string;
   startDate?: string;
   endDate?: string;
   officeId?: string;
-  type?: PresenceType;
+  remote?: boolean;
   userGroup?: string;
 };
