45d5d2f36bf57800ec01cb22e7ea30e8a0c82111
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2025-01-03 09:15:30 +0200

    Add constant presence selects to modal

--- app/src/gui/home-tab/views/settings/constant-presence-management/constant-presence-management.module.ts
+++ app/src/gui/home-tab/views/settings/constant-presence-management/constant-presence-management.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { OfficeModule } from "../../../../../entities/office/office.module";
 import { ConstantPresenceManagementController } from "./constant-presence-management.controller";
 import { ConstantPresenceManagementModal } from "./constant-presence-management.modal";
 
 @Module({
+  imports: [OfficeModule],
   providers: [ConstantPresenceManagementModal],
   controllers: [ConstantPresenceManagementController],
 })
