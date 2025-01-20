cefae879e7ba2de29fec898b0679b0b00644d0ad
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2025-01-20 12:15:43 +0200

    Cache fetching all offices

--- app/src/entities/office/office.service.ts
+++ app/src/entities/office/office.service.ts
@@ -1,14 +1,25 @@
-import { Injectable } from "@nestjs/common";
+import { CACHE_MANAGER } from "@nestjs/cache-manager";
+import { Inject, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
+import { Cache } from "cache-manager";
+import { CacheKey } from "../../common/cache-key.enum";
 import { OfficeNotFoundException } from "../../common/exceptions/office-not-found.exception";
 import { Office, OfficeRepository } from "./office.model";
 
 @Injectable()
 export class OfficeService {
-  constructor(@InjectRepository(Office) private officeRepository: OfficeRepository) {}
+  constructor(
+    @InjectRepository(Office) private officeRepository: OfficeRepository,
+    @Inject(CACHE_MANAGER) private cache: Cache,
+  ) {}
 
+  /**
+   * Find all offices in database.
+   *
+   * This method is cached.
+   */
   async findAll() {
-    return this.officeRepository.find();
+    return this.cache.wrap(CacheKey.ALL_OFFICES, async () => this.officeRepository.find());
   }
 
   async findById(id: string) {
@@ -20,14 +31,21 @@ export class OfficeService {
   }
 
   async create(name: string) {
+    await this.invalidateCache();
     return this.officeRepository.save({ name });
   }
 
   async update(id: string, name: string) {
+    await this.invalidateCache();
     return this.officeRepository.update({ id }, { name });
   }
 
   async delete(id: string) {
+    await this.invalidateCache();
     return this.officeRepository.delete({ id });
+  }
+
+  private async invalidateCache() {
+    return this.cache.del(CacheKey.ALL_OFFICES);
   }
 }
