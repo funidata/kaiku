8e3af248adf91f0a2248c09dbabe75e9e6726e73
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2025-01-20 13:45:33 +0200

    Move registration controller out of entity module

--- app/src/entities/presence/presence.module.ts
+++ app/src/entities/presence/presence.module.ts
@@ -1,14 +1,12 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { OfficeModule } from "../office/office.module";
-import { PresenceController } from "./presence.controller";
 import { Presence } from "./presence.model";
 import { PresenceService } from "./presence.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Presence]), OfficeModule],
   providers: [PresenceService],
-  controllers: [PresenceController],
   exports: [TypeOrmModule, PresenceService],
 })
 export class PresenceModule {}
--- app/src/gui/home-tab/views/registration/registration-view.module.ts
+++ app/src/gui/home-tab/views/registration/registration-view.module.ts
@@ -4,11 +4,13 @@ import { OfficeModule } from "../../../../entities/office/office.module";
 import { PresenceModule } from "../../../../entities/presence/presence.module";
 import { DayList } from "./day-list";
 import { DayListItem } from "./day-list-item";
+import { RegistrationController } from "./registration.controller";
 import { RegistrationView } from "./registration.view";
 
 @Module({
   imports: [OfficeModule, PresenceModule, ConstantPresenceModule],
   providers: [DayList, DayListItem, RegistrationView],
+  controllers: [RegistrationController],
   exports: [RegistrationView],
 })
 export class RegistrationViewModule {}
